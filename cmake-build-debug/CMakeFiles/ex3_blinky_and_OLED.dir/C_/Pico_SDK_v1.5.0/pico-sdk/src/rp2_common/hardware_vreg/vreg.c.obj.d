cfb3721df562eeae711a82d1edb23cd797c1fdcb
andreagy <andi.gyimoti@gmail.com>
2024-04-13 21:00:50 +0300

    uzenet

---
+++ cmake-build-debug/CMakeFiles/ex3_blinky_and_OLED.dir/C_/Pico_SDK_v1.5.0/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/ex3_blinky_and_OLED.dir/C_/Pico_SDK_v1.5.0/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj: \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2_common\hardware_vreg\vreg.c \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\assert.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\_ansi.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\newlib.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\_newlib_version.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\sys\config.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\machine\ieeefp.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\sys\features.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\stdint.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\machine\_default_types.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\sys\_intsup.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\sys\_stdint.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ D:\Metropolia_localsave\IoT_Connecting_Things\ex3_blinky_and_OLED\cmake-build-debug\generated\pico_base/pico/version.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico/config.h \
+ D:\Metropolia_localsave\IoT_Connecting_Things\ex3_blinky_and_OLED\cmake-build-debug\generated\pico_base/pico/config_autogen.h \
+ C:/Pico_SDK_v1.5.0/pico-sdk/src/boards/include/boards/pico_w.h \
+ C:/Pico_SDK_v1.5.0/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/addressmap.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/sio.h \
+ c:\pico_sdk_v1.5.0\gcc-arm-none-eabi\arm-none-eabi\include\sys\cdefs.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\common\pico_base\include/pico/error.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2_common\hardware_vreg\include/hardware/vreg.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/vreg_and_chip_reset.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2_common\hardware_base\include/hardware/address_mapped.h \
+ C:\Pico_SDK_v1.5.0\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/vreg_and_chip_reset.h
